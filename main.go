dd3ab243e0e2b098e3e7f8ca57a793a26c6082e9
Josh Daghlian <josh@daghlian.net>
2017-10-26 23:56:53 -0400

    now with CORS filters, visiting http://127.0.0.1:8000 actually manages to execute the XMLHttpRequest without trouble.  good times.

--- main.go
+++ main.go
@@ -11,20 +11,31 @@ var (
     port = flag.Int("port", 8000, "http listen port")
 )
 
+type CorsHeaderResponseFilter struct {
+}
+
+func (f *CorsHeaderResponseFilter) FilterResponse(req *falcore.Request, res *http.Response){
+     fmt.Println("setting a response header")
+     res.Header.Set("Access-Control-Allow-Origin", "http://127.0.0.1:8000")
+}
+
+
 func main() {
      flag.Parse()
 
      pipeline := falcore.NewPipeline()
 
      pipeline.Upstream.PushBack(helloFilter)
+     pipeline.Downstream.PushBack(corsFilter)
 
      server := falcore.NewServer(*port, pipeline)
 
      if err := server.ListenAndServe(); err != nil {
      	fmt.Println("failed to start server:", err)
      }
-     
 }
+
+var corsFilter = new(CorsHeaderResponseFilter)
 
 var helloFilter = falcore.NewRequestFilter(func(req *falcore.Request) *http.Response {
     return falcore.StringResponse(req.HttpRequest, 200, nil, `
@@ -33,8 +44,9 @@ var helloFilter = falcore.NewRequestFilter(func(req *falcore.Request) *http.Resp
     <p>For <em>some</em> kind of test, visit 127.0.0.1:8000 and note in the browser's dev console that localhost:8000 doesn't bload</p>
     <script>
       var r = new XMLHttpRequest();
-      r.addEventListener("load", function() { console.log(this.responseText); });
-      r.open("GET", "localhost:8000");
+      r.addEventListener("load", function() { console.log("monkey: "+this.responseText); });
+      r.open("GET", "http://localhost:8000");
+      r.setRequestHeader("Content-Type", "text/plain")
       r.send();
     </script>
   </body>
